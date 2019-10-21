9608edaf5b5d4fea28c1c2680c9a187cd62b55f5
staskolkovsky <stas.kolkovsky@gmail.com>
2019-10-21 22:40:42 +0300

    Modify view of template timatable-version2

--- src/assets/stub-server/stub-server.js
+++ src/assets/stub-server/stub-server.js
@@ -6,7 +6,6 @@ const importFile = require("../mocks/import");
 server.post("/api/v1/ttp-administrator/time-tables/import", (request, response) => {
   response.header("Content-Type", "application/json");
   response.send(JSON.stringify(importFile));
-  //throw new Error("dfdf");
 });
 
 server.listen(listenPort, () => {
