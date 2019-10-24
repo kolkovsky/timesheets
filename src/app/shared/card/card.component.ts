436708e3c22f7a14d2803b9a687db0a43ab5a580
staskolkovsky <stas.kolkovsky@gmail.com>
2019-10-24 12:41:16 +0300

    Create animation for cards [TTP][UI]-56

--- src/app/shared/card/card.component.ts
+++ src/app/shared/card/card.component.ts
@@ -1,15 +1,30 @@
-import {Component, Input, OnInit} from "@angular/core";
+import {Component, HostListener, Input, OnInit} from "@angular/core";
+import {cardAnimation} from "./card.animation";
 
 @Component({
   selector: "ttp-shared-card",
   templateUrl: "./card.component.html",
-  styleUrls: ["./card.component.less"]
+  styleUrls: ["./card.component.less"],
+  animations: [cardAnimation]
 })
 
 export class CardComponent implements OnInit {
 
   @Input() cardContent: string;
 
+  public show: boolean;
+
+  @HostListener('mouseover')
+  catchMouseOver() {
+    console.log("mouseover");
+    this.show = true;
+  }
+
+  @HostListener('mouseout')
+  catchMouseOut() {
+    console.log("mouseout");
+    this.show = false;
+  }
 
   ngOnInit(): void {
 
