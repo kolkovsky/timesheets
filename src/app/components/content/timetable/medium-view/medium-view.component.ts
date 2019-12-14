809d4ca0dd98dfa9df2516f1af4a140086d32297
staskolkovsky <stas.kolkovsky@gmail.com>
2019-12-14 23:30:56 +0300

    Create medium mode https://trello.com/c/grETXq5D/17-create-medium-mode

---
+++ src/app/components/content/timetable/medium-view/medium-view.component.ts
@@ -0,0 +1,11 @@
+import {Component} from "@angular/core";
+
+@Component({
+  selector: 'medium-view',
+  templateUrl: './medium-view.component.html',
+  styleUrls: ['./medium-view.component.less']
+})
+
+export class MediumViewComponent {
+
+}
